0b3d0cf9e70c83a18af109abb7a9529a04c53781
Diko99 <52924090+Diko99@users.noreply.github.com>
2020-01-22 10:40:18 +0700

    Add files via upload

---
+++ case5.js
@@ -0,0 +1,28 @@
+/* case 4 (array & integer)
+menghapus nilai yang sama
+data: [23, 45, 67, 90, 23, 1, 6, 90, 12, 43, 90, 67]
+perintah: jika kita menemukan nilai yang sama maka hapus nilai tersebut dan sisakan satu nilai yang tersisa
+output yang diharapkan:
+[23, 45, 67, 90, 1, 6, 12, 43] */
+
+
+let data = [10, 8, 2, 1, 3, 2, 0]
+let dataLength = data.length
+
+let res = []
+
+for (let i = 0; i < dataLength; i++) {
+  if (res.indexOf(data[i]) == -1) {
+    res.push(data[i])
+  }    
+}
+console.log("remove array duplicate => " + res.join(" | "))
+
+function sortNumber(c, b) {
+  return c - b;
+  console.log(c, b)
+}
+let out = res.sort();
+
+console.log(out);
+
